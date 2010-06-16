53426abea36cca183c1be3a5d97fbb88df207788
TangChao.ZJU <TangChao.ZJU@64fb52d0-08b1-11df-854a-816b00dff800>
2010-06-16 12:40:11 +0000

    修改了新的footer

--- trunk/wptheme-indienation/footer.php
+++ trunk/wptheme-indienation/footer.php
@@ -10,6 +10,26 @@
 		'<a href="http://wordpress.org/">WordPress</a>'); ?>
 		<br /><?php printf(__('%1$s and %2$s.'), '<a href="' . get_bloginfo('rss2_url') . '">' . __('Entries (RSS)') . '</a>', '<a href="' . get_bloginfo('comments_rss2_url') . '">' . __('Comments (RSS)') . '</a>'); ?>
 	</p>-->
+    <div class="bottom">
+        <ul>
+            <li class="first">
+                <a href="#">Returns & Exchange</a>
+            </li>
+            <li>
+                <a href="#">Privacy Police</a>
+            </li>
+            <li>
+                <a href="#">Terms & Conditions of Use</a>
+            </li>
+            <li>
+                <a href="#">My Account</a>
+            </li>
+            <li>
+                <a href="#">Powered by Volusion</a>
+            </li>
+        </ul>
+    </div>
+    <div class="bot-logo"><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logosmall.png" alt="INDIENATION CULTURE"/></a></div>
 </div>
 </div>
 
