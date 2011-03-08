e37bd5f59a33b871f472f4cc6502015167feb8c9
TangChao.ZJU <TangChao.ZJU@64fb52d0-08b1-11df-854a-816b00dff800>
2011-03-08 15:38:42 +0000

    create user,tag,post,comment,lookup model and post, comment crud

--- yiiblog/protected/config/main.php
+++ yiiblog/protected/config/main.php
@@ -20,12 +20,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'123456',
 		),
-		*/
+		
 	),
 
 	// application components
@@ -44,20 +44,20 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=127.0.0.1;dbname=yiiblog',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'abc',
+			'password' => '12345',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
