18340f3dc7f2fd1c1862d781d5e8e08cc15af4ec
tangchao.zju <tangchao.zju@64fb52d0-08b1-11df-854a-816b00dff800>
2011-06-13 12:39:02 +0000

    add lib oauth

---
+++ findzoombies/lib/oauth/config.php
@@ -0,0 +1,5 @@
+<?php
+define("MB_AKEY","3317d108416841f5a20c859bbfd37910"); //这里的'*********'请改成你的App Key
+define("MB_SKEY","9a41cfaf18d131f30e314a0abca705f0"); //这里的'*********'请改成你的App Secret
+define( "MB_RETURN_FORMAT" , 'json' );
+define( "MB_API_HOST" , 'open.t.qq.com' );
